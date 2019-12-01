d2df2f868c00d213aa4a8aa9a0c7e5456da001e1
Allkrain <aukra25@yandex.ru>
2019-12-01 09:49:50 +0300

    after security

--- app/blocks/rating/rating.js
+++ app/blocks/rating/rating.js
@@ -2,8 +2,8 @@ const $ = window.$;
 
 $(document).on('change', '.js-rating-control', function () {
   $(this)
-      .parent('.rating__box')
-      .addClass('is-active')
-      .siblings()
-      .removeClass('is-active');
+    .parent('.rating__box')
+    .addClass('is-active')
+    .siblings()
+    .removeClass('is-active');
 });
--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -6,8 +6,22 @@ const changed = require('gulp-changed');
 
 module.exports = () => (
   gulp.src('app/static/images/**/*')
-  .pipe(plumber({ errorHandler: errorHandler('Error in icons task') }))
+  .pipe(plumber({
+    errorHandler: errorHandler('Error in icons task')
+  }))
   .pipe(changed('dist/assets/images'))
-  .pipe(imagemin())
+  .pipe(imagemin([
+    imagemin.gifsicle({
+      interlaced: true,
+    }),
+    imagemin.jpegtran({
+      progressive: true,
+    }),
+    imagemin.optipng({
+      optimizationLevel: 5,
+    }),
+  ], {
+    verbose: true,
+  }))
   .pipe(gulp.dest('dist/assets/images'))
 );
